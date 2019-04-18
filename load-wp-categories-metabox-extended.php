0a1e59beabc30012269dfb6b2f871f519a8595d1
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2019-04-18 12:00:36 +0530

    Rename to shorten the var name

--- load-wp-categories-metabox-extended.php
+++ load-wp-categories-metabox-extended.php
@@ -5,19 +5,20 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 /**
  * Load WP Categories Metabox Extended plugin.
  *
+ * @since 1.1.0 Renamed variable to `$wpcme` for convenience.
  * @since 1.0.0
  *
  * @param string $plugin_file Main plugin file.
  */
 function wpcme_load_wp_categories_metabox_extended( $plugin_file ) {
-	global $wp_categories_metabox_extended;
+	global $wpcme;
 
 	$plugin_dir = plugin_dir_path( $plugin_file );
 
 	// Setup autoloader.
-	require_once 'include/WPCategoriesMetaboxExtendedAutoLoader.php';
+	require_once 'include/AutoLoader.php';
 
-	$loader = new \WPCategoriesMetaboxExtended\WPCategoriesMetaboxExtendedAutoLoader();
+	$loader = new \WPCategoriesMetaboxExtended\AutoLoader();
 	$loader->add_namespace( 'WPCategoriesMetaboxExtended', $plugin_dir . 'include' );
 
 	if ( file_exists( $plugin_dir . 'tests/' ) ) {
@@ -27,14 +28,14 @@ function wpcme_load_wp_categories_metabox_extended( $plugin_file ) {
 
 	$loader->register();
 
-	if ( ! isset( $wp_categories_metabox_extended ) ) {
-		$ui_manager          = new \WPCategoriesMetaboxExtended\UI\UIManager();
-		$term_helper         = new \WPCategoriesMetaboxExtended\Core\Utilities\TermHelper();
+	if ( ! isset( $wpcme ) ) {
+		$ui_manager  = new \WPCategoriesMetaboxExtended\UI\UIManager();
+		$term_helper = new \WPCategoriesMetaboxExtended\Core\Utilities\TermHelper();
 
-		$wp_categories_metabox_extended = new \WPCategoriesMetaboxExtended\Core\WPCategoriesMetaboxExtended( $plugin_file, $ui_manager, $term_helper );
-		$wp_categories_metabox_extended->add_loadie( new \WPCategoriesMetaboxExtended\Core\Request\WPCategoriesMetaboxExtendedAction() );
+		$wpcme = new \WPCategoriesMetaboxExtended\Core\WPCategoriesMetaboxExtended( $plugin_file, $ui_manager, $term_helper );
+		$wpcme->add_loadie( new \WPCategoriesMetaboxExtended\Core\Request\WPCategoriesMetaboxExtendedAction() );
 
-		add_action( 'plugins_loaded', array( $wp_categories_metabox_extended, 'load' ) );
+		add_action( 'plugins_loaded', array( $wpcme, 'load' ) );
 	}
 
 }
@@ -43,6 +44,7 @@ function wpcme_load_wp_categories_metabox_extended( $plugin_file ) {
  * Return the global instance of Email Log plugin.
  * Eventually the EmailLog class might become singleton.
  *
+ * @since 1.1.0 Renamed variable to `$wpcme` for convenience.
  * @since 1.0.0
  *
  * @global \WPCategoriesMetaboxExtended\core\WPCategoriesMetaboxExtended $search_categories_metabox
@@ -50,7 +52,7 @@ function wpcme_load_wp_categories_metabox_extended( $plugin_file ) {
  * @return \WPCategoriesMetaboxExtended\core\WPCategoriesMetaboxExtended
  */
 function wp_categories_metabox_extended() {
-	global $wp_categories_metabox_extended;
+	global $wpcme;
 
-	return $wp_categories_metabox_extended;
+	return $wpcme;
 }
